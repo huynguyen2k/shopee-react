433e633818ef39889051b25e899fabd14ed68787
huynguyen2k <huynguyen235023@gmail.com>
2023-08-28 22:10:58 +0700

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ProductList } from '@/components/ProductList'
+import { StudentList } from '@/components/StudentList'
+
+function App() {
+  return (
+    <div>
+      <ProductList />
+      <StudentList />
+    </div>
+  )
+}
+
+export default App
