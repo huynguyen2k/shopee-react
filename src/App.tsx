29a5901b2c32bc1d01274596c7c790e02a893903
huynguyen2k <huynguyen235023@gmail.com>
2023-09-25 21:59:25 +0700

    feat: test 123456

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import { ProductList } from '@/components/ProductList'
 import { StudentList } from '@/components/StudentList'
 
 function App() {
-  const a = 10
-
-  console.log(a)
-
   return (
     <div>
       <ProductList />
