9bbca66438f5daa4ec9ca20953b7d9400a53fb91
huynguyen2k <huynguyen235023@gmail.com>
2024-06-25 20:00:24 +0700

    feat: test

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,10 @@ import { Outlet } from 'react-router-dom'
 import { ResponsiveProvider } from '@/components/responsive'
 
 function App() {
+  const a = 10
+
+  console.log(a)
+
   return (
     <ResponsiveProvider>
       <Outlet />
