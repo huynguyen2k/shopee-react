24c3ba0a454a3c616c0a2beff408d92a287c50ae
huynguyen2k <huynguyen235023@gmail.com>
2025-05-18 11:09:36 +0700

    feat: building useTheme hook

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router/dom'
 
-import { router } from '@/configs/router'
-// global styles
+// we need to import the SCSS file before the router to make sure
+// the main styles are applied before the router styles when build the project
 import '@/styles/main.scss'
+// eslint-disable-next-line import/order
+import { router } from '@/configs/router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,26 @@
+import { Link } from 'react-router'
+
 import { Pagination } from '@/components/pagination'
 import { BlogList } from '@/features/auth/components/blog-list'
 import { blogData } from '@/features/auth/mocks/data'
+import { useTheme } from '@/hooks/use-theme'
 
 import classes from './styles.module.scss'
 
 export function Home() {
-  return (
-    <div style={{ padding: '32px' }}>
-      <h1 className={classes.title}>Home</h1>
-      <BlogList data={blogData} />
+  const [theme, toggleTheme] = useTheme()
 
+  return (
+    <div className={classes.home}>
+      <h1 className={classes.title}>Home</h1>
+      <button type="button" onClick={toggleTheme}>
+        {theme === 'light-theme'
+          ? 'Switch to Dark Theme'
+          : 'Switch to Light Theme'}
+      </button>
+      <Link to="/login">Login</Link>
+
+      <BlogList data={blogData} />
       <Pagination page={1} totalPages={10} />
     </div>
   )
